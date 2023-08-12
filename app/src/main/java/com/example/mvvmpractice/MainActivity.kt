e9c14c7237eb6206016adba5490b2c53d9b22676
Sajib Mamun <sajibmamun138@gmail.com>
2023-08-13 01:38:21 +0600

    binding & basic part done

--- app/src/main/java/com/example/mvvmpractice/MainActivity.kt
+++ app/src/main/java/com/example/mvvmpractice/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.mvvmpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var value:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.incrementbtnid.setOnClickListener {
+
+            value++
+
+            binding.resulttvid.text= "$value"
+
+
+        }
+
+
+
     }
 }
